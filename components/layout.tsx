4bb5dfbc049c742cc146fbe449f0d6ce41cbe509
chrissiemhrk <chrissiemhrk@gmail.com>
2022-04-16 09:27:57 +0200

    add header

---
+++ components/layout.tsx
@@ -0,0 +1,74 @@
+import React, { ReactChild, useState } from 'react'
+import Image from 'next/image'
+import Logo from '../public/logo.svg'
+import Menu from '../public/menu.svg'
+import Link from 'next/link'
+interface Props {
+  children: ReactChild
+}
+
+const Layout = ({ children }: Props) => {
+  const [isOpen, setIsOpen] = useState(false)
+  return (
+    <>
+      <header className="flex items-center justify-between bg-linen px-5 py-5 lg:px-40">
+        <Link href={'/'}>
+          <a>
+            <Image src={Logo} alt="logo" width={70} height={70} />
+          </a>
+        </Link>
+        <nav className="hidden lg:flex lg:w-52 lg:items-center lg:justify-between">
+          <Link href={'#'}>
+            <a className="font-bold">Episodes</a>
+          </Link>
+          <Link href={'#'}>
+            <a className="font-bold">About</a>
+          </Link>
+          <Link href={'#'}>
+            <a className="font-bold">Blog</a>
+          </Link>
+        </nav>
+        <div className="hidden lg:flex lg:w-80 lg:items-center  lg:justify-between">
+          <button className="text-red-500 rounded-lg border-2 border-black px-5 py-2 font-bold">
+            Recent Episodes
+          </button>
+          <button className="rounded-lg bg-black px-6 py-2 font-bold text-white shadow-subscribeBtn">
+            Subscribe
+          </button>
+        </div>
+
+        <button onClick={() => setIsOpen(!isOpen)} className="block lg:hidden">
+          <Image src={Menu} alt="menu" width={30} height={30} />
+        </button>
+
+        {isOpen && (
+          <div className="absolute top-28 left-0 w-full bg-linen px-6 py-5 lg:hidden">
+            <nav className="flex flex-col text-lg">
+              <Link href={'#'}>
+                <a className="mb-3 font-bold">Episodes</a>
+              </Link>
+              <Link href={'#'}>
+                <a className="mb-3 font-bold">About</a>
+              </Link>
+              <Link href={'#'}>
+                <a className="mb-3 font-bold">Blog</a>
+              </Link>
+            </nav>
+            <div className="mt-5 flex flex-col">
+              <button className="text-red-500 mb-5 rounded-lg border-2 border-black px-5 py-2 font-bold">
+                Recent Episodes
+              </button>
+              <button className="rounded-lg bg-black px-6 py-2 font-bold text-white shadow-subscribeBtn">
+                Subscribe
+              </button>
+            </div>
+          </div>
+        )}
+      </header>
+      <main>{children}</main>
+      <footer></footer>
+    </>
+  )
+}
+
+export default Layout
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    colors: {
+      linen: '#F7EDE8',
+      black: '#000000',
+      white: '#FFFFFF',
+      vermillion: '#CD4631',
+      gray: '#646464',
+    },
+    extend: {
+      boxShadow: {
+        subscribeBtn: '3px 3px 0px 1px rgba(180,180,180,1)',
+      },
+    },
   },
   plugins: [],
 }
